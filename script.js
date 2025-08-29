d93e811ca92c6f42a213e264e3a7ffff62db0e09
Pritty Biswas <prittybiswas@gmail.com>
2025-08-29 21:41:24 +0530

    Added javascript file and wrote some line of code

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.getElementById("cardContainer");
+const historyList = document.getElementById("historyList");
+const clearHistory = document.getElementById("clearHistory");
+
+let heartCount = 0;
+let copyCount =0;
+
+hotline.forEach(service =>{
+    const card = document.createElement("div");
+    card.className ="w-54 rounded-xl shadow-md border p-4 flex flex-col gap-3 relative bg-white";
+
+
+    
+})
